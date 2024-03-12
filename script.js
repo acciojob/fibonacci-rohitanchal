853f882ea3de3d43bf7fde51206b9203bf5eaadd
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2024-03-12 23:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function fibonacci(num) {
-// your code here
-	if (num === 0) {
-        return 0;
+    if (num <= 0) {
+        return "Invalid input. Please provide a positive integer greater than 0.";
     } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
         return 1;
     } else {
-        let a = 0;
-        let b = 1;
+        let fibSequence = [0, 1];
         for (let i = 2; i < num; i++) {
-            const temp = b;
-            b = a + b;
-            a = temp;
+            let nextFib = fibSequence[i - 1] + fibSequence[i - 2];
+            fibSequence.push(nextFib);
         }
-        return b;
-	}
+        return fibSequence[fibSequence.length - 1];
+    }
 }
 
 module.exports = fibonacci;
