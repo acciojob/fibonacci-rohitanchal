8e2d52202d0a55c3da158dd8d488d9fb7580cfc4
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2024-03-12 23:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        let a = 0;
+        let b = 1;
+        for (let i = 2; i < num; i++) {
+            const temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+	}
 }
 
 module.exports = fibonacci;
